597817bcc7c0e11c7f09ebe764d2c6f450df9d24
Jasmine Lim <jasmine_lim13@yahoo.com>
2023-05-16 09:47:09 -0500

    inital changes

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -7,9 +7,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Exercises: Angular Lesson 3';
-
-  color = 'green';
+  color = "green";
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
+
+
+  handleTakeOff() {
+    let result = window.confirm('Confirm that the shuttle is ready for takeoff.');
+    if (result) {
+      this.color = 'blue';
+      this.height += 10000;
+      this.message = 'Shuttle in flight.';
+    }
+  }
+  
+
+ handleLand() {
+  window.alert('The shuttle is landing. Landing gear engaged.');
+  this.color = 'green';
+  this.height = 0;
+  this.message = 'The shuttle has landed.';
+}
+
+
+handleAbort() {
+  let result = window.confirm('Are you sure you want to abort the mission?');
+  if (result) {
+    this.color = 'red';
+    this.height = 0;
+    this.message = 'Mission aborted.';
+  }
+}
+
+
+moveRocket(rocketImage: HTMLElement, direction: string) {
+  if (direction === 'up') {
+    this.height += 10000;
+  } else if (direction === 'down') {
+    this.height -= 10000;
+  } else if (direction === 'right') {
+    let movement = parseInt(rocketImage.style.left || '0') + 10;
+    rocketImage.style.left = movement + 'px';
+    this.width += 10000;
+  } else if (direction === 'left') {
+    let movement = parseInt(rocketImage.style.left || '0') - 10;
+    rocketImage.style.left = movement + 'px';
+    this.width -= 10000;
+  }
+}
+
+
 }
